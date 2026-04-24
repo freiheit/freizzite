f369aaafc1c2eaa239324e05f0a5b34d606d8913
freiheit <git@freiheit.wtf>
2026-04-23 22:24:44 -0700

    fix: get 1password back and get completions to work

--- build_files/build-desktop.sh
+++ build_files/build-desktop.sh
@@ -10,6 +10,10 @@ dnf5 -y copr enable atim/starship
 rpm --import https://packages.microsoft.com/keys/microsoft.asc &&
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo
 
+# https://support.1password.com/install-linux/#fedora-or-red-hat-enterprise-linux
+rpm --import https://downloads.1password.com/linux/keys/1password.asc
+echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo
+
 DESKTOP_PACKAGES=(
     # 1Password (Integrations struggle in Flatpak install)
     1password
@@ -70,10 +74,11 @@ dnf -y install --skip-unavailable "${DESKTOP_PACKAGES[@]}"
 
 systemctl enable "${DESKTOP_SYSTEMCTL[@]}"
 
-op completion fish > /etc/fish/completions/op
-op completion bash > /etc/bash_completion.d/op
+# redirect $HOME because starship and 1password/op really like to write cache files under /root/
+HOME=/var/tmp starship completions fish > /etc/fish/completions/starship
+HOME=/var/tmp starship completions bash > /etc/bash_completion.d/starship
 
-starship completion fish > /etc/fish/completions/starship
-starship completion bash > /etc/bash_completion.d/starship
+HOME=/var/tmp op --cache=false completion fish > /etc/fish/completions/op
+HOME=/var/tmp op --cache=false completion bash > /etc/bash_completion.d/op
 
 npm completion > /etc/bash_completion.d/npm
